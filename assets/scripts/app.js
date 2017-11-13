cb7efcfe2490c01b824bcad510de7554beae6f4b
3poitn14guy <sunshine.farm.nc@gmail.com>
2017-11-13 15:38:02 -0500

    brought in files from MCTTS project

---
+++ assets/scripts/app.js
@@ -0,0 +1,11 @@
+import css from './app.scss';
+
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax () {
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
